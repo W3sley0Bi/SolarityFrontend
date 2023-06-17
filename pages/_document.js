0df992f4b1d7ca216afa1d535fc3b3644f318b10
salehghraiyib <saleh.ghrayeb@gmail.com>
2023-06-17 19:52:25 +0200

    Fix map marker

--- pages/_document.js
+++ pages/_document.js
@@ -13,7 +13,9 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head>{CssBaseline.flush()}</Head>
+        <Head>{CssBaseline.flush()}
+        </Head>
+        <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css" integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY=" crossorigin="" />
         <body>
           <Main />
           <NextScript />
